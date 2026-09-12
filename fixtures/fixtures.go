ee4e6e0daa8f8642d0024871dfb727ed39cc6d11
syed-imam <simam@crystalknows.com>
2026-09-12 03:22:56 -0400

    Add github action workflow

--- fixtures/fixtures.go
+++ fixtures/fixtures.go
@@ -2,6 +2,19 @@ package fixtures
 
 import "elle-go/history"
 
+func WriteSkew() history.History {
+	return history.History{
+		{Process: 1, Type: history.Ok, Mops: []history.Mop{
+			{Type: history.Read, Key: "y", Read: []int{}},
+			{Type: history.Append, Key: "x", App: 1},
+		}},
+		{Process: 2, Type: history.Ok, Mops: []history.Mop{
+			{Type: history.Read, Key: "x", Read: []int{}},
+			{Type: history.Append, Key: "y", App: 1},
+		}},
+	}
+}
+
 func Serializable() history.History {
 	return history.History{
 		{Process: 1, Type: history.Ok, Mops: []history.Mop{
